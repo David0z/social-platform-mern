f614ce2582bd9657d0528fc8cadf80fca06d0e11
David0z <dawid5soon5@gmail.com>
2022-05-02 17:22:21 +0200

    Added basic files and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import NotFound from './pages/404/NotFound';
+import Login from './pages/login/Login';
+import Post from './pages/post/Post';
+import Posts from './pages/posts/Posts';
+import SignUp from './pages/sign-up/SignUp';
+import User from './pages/user/User';
+import WelcomePage from './pages/welcome-page/WelcomePage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>HELLO WORLD</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<WelcomePage />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/posts' element={<Posts />} />
+        <Route path='/posts/:id' element={<Post />} />
+        <Route path='/users/:id' element={<User />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
