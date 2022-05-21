97b40c0d1b2484f4e1c318cdb16fe8962269addc
David0z <dawid5soon5@gmail.com>
2022-05-22 01:41:47 +0200

    styled votes list modal content

--- frontend/src/components/votes-list/VotesList.jsx
+++ frontend/src/components/votes-list/VotesList.jsx
@@ -1,11 +1,36 @@
-import styles from './VotesList.module.scss'
+import styles from "./VotesList.module.scss";
+import { useState } from "react";
 
 const VotesList = ({ postId }) => {
-  return (
-    <div className={styles.wrapper}>
-      
-    </div>
-  )
-}
+  const [activeType, setActiveType] = useState("upvotes");
 
-export default VotesList
+  return (
+    <div className={styles.wrapper} onClick={(e) => e.stopPropagation()}>
+      <div>
+        <div className={styles.buttons__wrapper}>
+          <button
+            className={styles.button}
+            onClick={() => setActiveType("upvotes")}
+          >
+            Upvotes
+          </button>
+          <button
+            className={styles.button}
+            onClick={() => setActiveType("downvotes")}
+          >
+            Downvotes
+          </button>
+        </div>
+        <div
+          className={`${styles.indicator} ${
+            activeType === "upvotes"
+              ? styles.indicator__upvote
+              : styles.indicator__downvote
+          }`}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default VotesList;
