c1222b9ed5c9a4c4a574c7e9ca85f477953e51e1
David0z <dawid5soon5@gmail.com>
2022-07-05 20:01:40 +0200

    Added pagination hook

---
+++ frontend/src/hooks/usePagination.js
@@ -0,0 +1,24 @@
+import { useState, useRef, useCallback } from "react";
+
+const usePagination = (hasMore, isLoading) => {
+  const observer = useRef();
+  const [page, setPage] = useState(0);
+  const lastPostElementRef = useCallback(
+    (node) => {
+      if (isLoading) return;
+
+      if (observer.current) observer.current.disconnect();
+
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPage((prevPageNumber) => prevPageNumber + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [isLoading]
+  );
+  return { page, lastPostElementRef }
+}
+
+export default usePagination
--- frontend/src/pages/posts/PostsAll.jsx
+++ frontend/src/pages/posts/PostsAll.jsx
@@ -4,28 +4,12 @@ import { getAllPosts, postActions } from "../../store/post/postSlice";
 import PostsList from "../../components/posts-list/PostsList";
 import PostSkeletonList from "../../components/skeletons/PostSkeletonList";
 import styles from "./Posts.module.scss";
+import usePagination from "../../hooks/usePagination";
 
 const PostsAll = () => {
   const dispatch = useDispatch();
   const { posts, isLoading, hasMore } = useSelector((state) => state.post.posts);
-
-  const observer = useRef();
-  const [page, setPage] = useState(0);
-  const lastPostElementRef = useCallback(
-    (node) => {
-      if (isLoading) return;
-
-      if (observer.current) observer.current.disconnect();
-
-      observer.current = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting && hasMore) {
-          setPage((prevPageNumber) => prevPageNumber + 1);
-        }
-      });
-      if (node) observer.current.observe(node);
-    },
-    [isLoading]
-  );
+  const { page, lastPostElementRef } = usePagination(hasMore, isLoading);
 
   useEffect(() => {
     dispatch(getAllPosts(page));
